5a182f574a461ef71d0c9b97547f2abc93f1e192
Duong1009 <duongb2014559@student.ctu.edu.vn>
2024-01-12 08:28:46 +0700

    dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const contactsRouter = require("/app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -2,10 +2,14 @@ const express = require("express");
 const contacts = require("../controllers/contact.controller");
 const router = express.Router();
 
-router.route("/").get(contacts.findAll).post(contacts.create).delete(contacts.deleteAll);
+router.route("/").get(contacts.findAll);
+router.route("/").post(contacts.create);
+router.route("/").delete(contacts.deleteAll);
 
 router.route("/favorite").get(contacts.findAllFavorite);
 
-router.route("/:id").get(contacts.findOne).put(contacts.update).delete(contacts.delete);
+router.route("/:id").get(contacts.findOne);
+router.route("/:id").put(contacts.update);
+router.route("/:id").delete(contacts.delete);
 
 module.exports = router;
